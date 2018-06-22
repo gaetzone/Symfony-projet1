aee5c155c275aa8f1fb8336c5974886d25bce7d4
gaetan <g.lepage.pro@gmail.com>
2018-06-22 16:34:48 +0200

    Utilisaton de la classe Response

--- src/Controller/Lucky2Controller.php
+++ src/Controller/Lucky2Controller.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class Lucky2Controller extends Controller
 {
@@ -17,6 +17,7 @@ class Lucky2Controller extends Controller
     {      
         // $slug = partie dynamique de l'URL (ex. pour /blog/aaa, $slug='aaa'
         echo 'Methode toto()';
+        
         return $this->render('lucky2/index.html.twig', [
             'controller_name' => 'Lucky2Controller',
             'slug' => $slug,
@@ -25,11 +26,26 @@ class Lucky2Controller extends Controller
     
     public function tata($year, $slug)
     {
-        echo 'Methode tata()';
+        echo 'Methode tata() <br>';
+        
+        // Création de données au format json
+        $status = Response::$statusTexts;
+        $json_data = $this->json($status);
+        
+        // Affichage du resultat de la requete http
+        $http_response = new Response();
+        echo $http_response; echo '<br>';
+        
+        // Affichage du header de la requete http
+        echo $http_response->headers; echo '<br>';
+        // Affichage du code d'etat de la requete http
+        echo $http_response->getStatusCode(); echo '<br>';
+        
         return $this->render('lucky2/index.html.twig', [
             'controller_name' => 'Lucky2Controller',
             'year' => $year,
             'slug' => $slug,
+            'json_data' => $json_data,
         ]);
     }
 
