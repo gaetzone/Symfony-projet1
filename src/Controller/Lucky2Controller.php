93c9f8bcb249760977addcbdd6e1e462c716f3c0
gaetan <g.lepage.pro@gmail.com>
2018-06-22 15:36:19 +0200

    Travail sur les routes
    1 route = map entre 1 url et 1 controller et la methode adéquate
    CONTROLLER_CLASS::METHOD

--- src/Controller/Lucky2Controller.php
+++ src/Controller/Lucky2Controller.php
@@ -2,18 +2,35 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class Lucky2Controller extends Controller
 {
     /**
-     * @Route("/lucky2", name="lucky2")
+     * Matches /lucky2/*
+     *
+     * @Route("/lucky2/{slug}", name="lucky2_show")
      */
-    public function index()
-    {
+    public function toto($slug = 'home')
+    {      
+        // $slug = partie dynamique de l'URL (ex. pour /blog/aaa, $slug='aaa'
+        echo 'Methode toto()';
         return $this->render('lucky2/index.html.twig', [
             'controller_name' => 'Lucky2Controller',
+            'slug' => $slug,
         ]);
     }
+    
+    public function tata($year, $slug)
+    {
+        echo 'Methode tata()';
+        return $this->render('lucky2/index.html.twig', [
+            'controller_name' => 'Lucky2Controller',
+            'year' => $year,
+            'slug' => $slug,
+        ]);
+    }
+
 }
