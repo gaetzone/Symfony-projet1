d882f89df9f95e70f3e1fe39610958d74817cb10
gaetan <g.lepage.pro@gmail.com>
2018-06-22 17:24:10 +0200

    Utilisation d'un Service (objet Symfony existant ou à ajouter)

--- src/Controller/Lucky1Controller.php
+++ src/Controller/Lucky1Controller.php
@@ -2,21 +2,34 @@
 namespace App\Controller;       // on spécifie le namespace à laquelle appartient la classe Lucky1Controller
 
 use Symfony\Component\HttpFoundation\Response;
+use Psr\Log\LoggerInterface;
 
 class Lucky1Controller
 {
     public function number()
-    {
+    {  
         $number = random_int(0, 100);
        
         return new Response(
             '<html><body>
-            
             Lucky number: '.$number.'
             <br>
             Réponse du serveur: code '.Response::HTTP_FOUND.'
-
+            <br>
             </body></html>'
         );
+    }
+    
+    public function getLoggerInterface(LoggerInterface $logger)
+    {
+        
+        echo $logger->info('helloooooo'); echo '<br>';
+        
+        return new Response(
+            '<html><body>
+                hello
+            
+            </body></html>'
+                );
     }
 }
