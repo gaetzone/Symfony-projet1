f52de897e11da2ab98f4de74dcb502c80231bded
gaetan <g.lepage.pro@gmail.com>
2018-06-22 16:09:38 +0200

    Accès à une CONST de l'objet Response

--- src/Controller/Lucky1Controller.php
+++ src/Controller/Lucky1Controller.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Controller;
+namespace App\Controller;       // on spécifie le namespace à laquelle appartient la classe Lucky1Controller
 
 use Symfony\Component\HttpFoundation\Response;
 
@@ -8,9 +8,15 @@ class Lucky1Controller
     public function number()
     {
         $number = random_int(0, 100);
-        
+       
         return new Response(
-                '<html><body>Lucky number: '.$number.'</body></html>'
-                );
+            '<html><body>
+            
+            Lucky number: '.$number.'
+            <br>
+            Réponse du serveur: code '.Response::HTTP_FOUND.'
+
+            </body></html>'
+        );
     }
 }
